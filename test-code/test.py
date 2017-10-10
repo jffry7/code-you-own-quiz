5ed1f1f401167bbb99189bde0ac29f9b87a64cb3
jffry7 <jffry7@gmail.com>
2017-10-10 17:25:02 +0800

    making atom work with python and execute thru package script

---
+++ test-code/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding=utf-8
+
+"""
+This is an example script.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
+
+print('Hello world!')
+# Python Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degC is equal to %0.1f degF' % (celsius, fahrenheit))
