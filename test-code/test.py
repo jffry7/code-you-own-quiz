4227818aaeadf5f9c30bc4ac3f85176fc100c161
jffry7 <jffry7@gmail.com>
2017-10-11 17:36:42 +0800

    python 2 code sample

--- test-code/test.py
+++ test-code/test.py
@@ -6,13 +6,13 @@ req D100 missing docstring.
 
 D200 more than one line required
 """
-
+import re
 print('Hello world!')
 # Python Program to convert temperature in celsius to fahrenheit
 
-# change this value for a different result
-degC = 37.5
 
-# calculate fahrenheit
-degF = (degC * 1.8) + 32
-print('%0.1f degC is equal to %0.1f degF' % (degC, degF))
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print test_string, 'is a valid US local phone number'
+    else:
+        print test_string, 'rejected'
