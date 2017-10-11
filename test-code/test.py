b2e479983db0c14df30bd5e307a18c21e4e6d15b
jffry7 <jffry7@gmail.com>
2017-10-11 22:35:19 +0800

    correct spacing

--- test-code/test.py
+++ test-code/test.py
@@ -9,8 +9,6 @@ D200 more than one line required
 import re
 print('Hello world!')
 # Python Program to convert temperature in celsius to fahrenheit
-
-
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
         print test_string, 'is a valid US local phone number'
