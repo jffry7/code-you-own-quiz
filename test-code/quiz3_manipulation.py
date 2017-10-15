87ce1840a03c203ee061e4c03e957228fc79d67c
jffry7 <jffry7@gmail.com>
2017-10-15 20:30:24 +0800

    added quiz3

---
+++ test-code/quiz3_manipulation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding=utf-8
+
+"""
+req D100 missing docstring.
+
+D200 more than one line required
+"""
+s = 'test'
+print ('a' + s)[1:]  # a+'string' then prints 1-end. it exclude a
+print s[0] + s[1:]  # will not work if s=''
+print s + ''  # it adds null ''
+print s[0:]  # it prints from first char and doesnt cause error for ''
