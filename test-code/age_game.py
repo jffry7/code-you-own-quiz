ba006ab980b5d462111a0596a49df26118e8f6ad
jffry7 <jffry7@gmail.com>
2017-10-19 15:59:34 +0800

    adjust the year factor

--- test-code/age_game.py
+++ test-code/age_game.py
@@ -12,10 +12,10 @@ def pause():
     m.getch()
 
 
-def split_number(number_value):
+def split_number(number_value, year_value):
     u"""Split xyz to x and yz."""
     # process the user output
-    number_value = str(1015 - number_value)
+    number_value = str(year_value - 1000 - number_value)
     # subtract value from control and coverts to string
     user_number = number_value[0]  # first string value
     user_age = number_value[1:]  # last 2 string value
@@ -24,6 +24,8 @@ def split_number(number_value):
 
 # ask for user identity
 user_name = raw_input("Who are you? ")
+# added the year factor as it will vary if played years after it was created
+current_year = int(raw_input("What is the current year? "))
 # start of the game
 print "You will need a calculator"
 pause()
@@ -39,4 +41,4 @@ print "finally subtract the answer from the year you were born"
 user_input_number = int(raw_input("What is the final answer: "))
 # output the processed numbers
 print
-print user_name.title() + "'s lucky number is", split_number(user_input_number)
+print user_name.title() + "'s lucky number is", split_number(user_input_number, current_year)
