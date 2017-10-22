65a3a8dad262973f3678d403595b92e5b892db72
jffry7 <jffry7@gmail.com>
2017-10-22 22:54:58 +0800

    print box around final output
    some issue uploading from ATOM manually adding direct from github
    
    added text box

--- test-code/age_game.py
+++ test-code/age_game.py
@@ -10,6 +10,20 @@ def pause():
     raw_input()
 
 
+def bar(n, ch="-"):
+    u"""Horizontal bar on text."""
+    # lesson 9 udacity
+    return ch * n
+
+
+def printbox(message):
+    u"""Box bar around text."""
+    # lesson 9 udacity
+    print "+-" + bar(len(message)) + "-+"
+    print "| " + message + " |"
+    print "+-" + bar(len(message)) + "-+"
+
+
 def split_number(number_value, year_value):
     u"""Split xyz to x and yz."""
     # process the user output
@@ -17,7 +31,7 @@ def split_number(number_value, year_value):
     # subtract value from control and coverts to string
     user_number = number_value[0]  # first string value
     user_age = number_value[1:]  # last 2 string value
-    return user_number + " and is " + user_age + " yrs old"
+    return user_name.title() + "'s lucky number is " + user_number + " and is " + user_age + " yrs old"
 
 
 # ask for user identity
@@ -25,7 +39,8 @@ user_name = raw_input("Who are you? ")
 # added the year factor as it will vary if played years after it was created
 current_year = int(raw_input("What is the current year? "))
 # start of the game
-print "You will need a calculator"
+print
+print "You may need a calculator"
 pause()
 print user_name.title(), "think of a number from 1 to 9"
 pause()
@@ -35,8 +50,12 @@ print "then add 50"
 pause()
 print "multiply it by 20"
 pause()
-print "finally subtract the answer from the year you were born"
+print "finally, deduct the answer from the year you were born"
 user_input_number = abs(int(raw_input("What is the final answer: ")))
 # output the processed numbers
 print
-print user_name.title() + "'s lucky number is", split_number(user_input_number, current_year)
+printbox(split_number(user_input_number, current_year))
+# print "+-" + bar(len(split_number(user_input_number, current_year))) + "-+"
+# print "| " + split_number(user_input_number, current_year) + " |"
+# print "+-" + bar(len(split_number(user_input_number, current_year))) + "-+"
+# print user_name.title() + "'s lucky number is", split_number(user_input_number, current_year)
