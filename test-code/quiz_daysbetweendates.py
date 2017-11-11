d3750f5ad0f30ceaa715f65c8e628dee70cbb836
jffry7 <jffry7@gmail.com>
2017-11-11 18:23:57 +0800

    added quiz answer

--- test-code/quiz_daysbetweendates.py
+++ test-code/quiz_daysbetweendates.py
@@ -21,19 +21,36 @@ def nextDay(year, month, day):
             return year, month + 1, 1
 
 
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False
+
+
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1."""
-    """   and year2/month2/day2. Assumes inputs are valid dates
+    """and year2/month2/day2. Assumes inputs are valid dates
        in Gregorian calendar, and the first date is not after
        the second."""
     # YOUR CODE HERE!
+    """
     counter = 0
     while year1 != year2 or month1 != month2 or day1 != day2:
         counter += 1
         year1, month1, day1 = nextDay(year1, month1, day1)
     # print current_year, current_month, current_day
     # print counter
-    return counter
+    return counter"""
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        days += 1
+        year1, month1, day1 = nextDay(year1, month1, day1)
+    return days
 
 
 def test():
