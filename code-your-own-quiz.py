baa496e869805c04c78b53512815177cc1844b55
jffry7 <jffry7@gmail.com>
2017-12-02 06:19:10 +0800

    trial game levels/difficulty test

--- code-your-own-quiz.py
+++ code-your-own-quiz.py
@@ -7,6 +7,8 @@
 # Game type: Fill in the blanks
 # Game level: 1 ~ 4 with 5 question in each level
 
+num_question_per_level = 4
+
 
 def pause(pause_message):
     """Add pause to the game with correct message."""
@@ -33,14 +35,54 @@ def game_difficulty():
     print "Choose the game dificulty"
     print "Total number of question is 4 times the game difficulty"
     game_level = raw_input("Choose from 1 to 4: ")
-    valid_levels = ["1", "2", "3", "4"]
-    for val_level in valid_levels:
-        print game_level, val_level
-        if game_level == val_level:
+    if game_level.isdigit():
+        game_level = int(game_level)
+        if game_level in range(1, 5):
             return game_level
+        else:
+            printbox("Out of range")
+            pause("Press Enter to input game difficulty")
+            game_difficulty()
     printbox("Invalid input")
     pause("Press Enter to input game dificulty")
     game_difficulty()
 
 
-print game_difficulty()
+def mistake_question():
+    mistakes = raw_input("How many mistake(s) before the game ends? ")
+    return mistakes
+
+
+def number_of_mistakes(game_level, num_quest):
+    """DO check max number of mistake."""
+    max_num_mistake = (num_quest * game_level) + 1
+    mistakes = mistake_question()
+    while not mistakes.isdigit():
+        printbox("Invalid input")
+        pause("Press Enter to input number of mistake(s)")
+        mistakes = mistake_question()
+    mistakes = int(mistakes)
+    while mistakes not in range(1, max_num_mistake):
+        print "range", mistakes
+        printbox("Invalid input")
+        pause("Press Enter to input number of mistake(s)")
+        mistakes = mistake_question()
+    return mistakes
+
+
+"""if mistakes.isdigit():
+        mistakes = int(mistakes)
+        if mistakes in range(1, max_num_mistake):
+            print "inside", mistakes
+            return mistakes
+        else:
+            printbox("Out of range")
+            pause("Press Enter to input number of mistake(s)")
+            number_of_mistakes(game_level, num_quest)
+    else:
+        printbox("Invalid input")
+        pause("Press Enter to input number of mistake(s)")
+        number_of_mistakes(game_level, num_quest)"""
+
+
+print number_of_mistakes(game_difficulty(), num_question_per_level)
