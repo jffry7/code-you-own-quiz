99ab1c4522c06d44a818a3eb0ea52d13ab93a2a0
jffry7 <jffry7@gmail.com>
2017-12-02 21:34:58 +0800

    change to single function for both game level and number of mistake

--- code-your-own-quiz.py
+++ code-your-own-quiz.py
@@ -8,6 +8,9 @@
 # Game level: 1 ~ 4 with 5 question in each level
 
 num_question_per_level = 4
+min_number = 1
+max_game_level = 4
+max_question = 4
 
 
 def pause(pause_message):
@@ -26,63 +29,41 @@ def printbox(message):
     """Box bar around text."""
     # lesson 9 udacity
     print "+-" + bar(len(message)) + "-+"
+    # prints dashes equal to length of variable message
     print "| " + message + " |"
     print "+-" + bar(len(message)) + "-+"
 
 
-def game_difficulty():
-    """Do check validity of game level and returns it."""
-    print "Choose the game dificulty"
-    print "Total number of question is 4 times the game difficulty"
-    game_level = raw_input("Choose from 1 to 4: ")
-    if game_level.isdigit():
-        game_level = int(game_level)
-        if game_level in range(1, 5):
-            return game_level
-        else:
-            printbox("Out of range")
-            pause("Press Enter to input game difficulty")
-            game_difficulty()
-    printbox("Invalid input")
-    pause("Press Enter to input game dificulty")
-    game_difficulty()
+def ask_question(question, target_range):
+    """Do ask the generic question and return an integer."""
+    # re-use the function to ask question
+    # validates input to required parameter
+    answer_to_question = raw_input(question)
+    while not answer_to_question.isdigit():
+        printbox(answer_to_question + " is not a digit")
+        pause("Press Enter to continue....")
+        answer_to_question = raw_input(question)
+    # check if input is a digit
+    answer_to_question = int(answer_to_question)
+    while answer_to_question not in range(min_number, target_range):
+        printbox("Out of range")
+        pause("Press Enter to continue....")
+        answer_to_question = raw_input(question)
+        answer_to_question = int(answer_to_question)
+    # Check if input is within the min max range
+    return answer_to_question
 
 
-def mistake_question():
-    mistakes = raw_input("How many mistake(s) before the game ends? ")
-    return mistakes
+def main():
+    print "Choose the game level"
+    game_level = ask_question("Choose game level from" + str(min_number) +
+                              " to " + str(max_game_level) + " : ", (max_game_level + 1))
+    print "Total number of question is " + str(num_question_per_level) + " times the game difficulty"
+    number_of_mistakes = ask_question("How many mistake(s) allowed before the game ends? ", (num_question_per_level * game_level))
+    return game_level, number_of_mistakes
 
 
-def number_of_mistakes(game_level, num_quest):
-    """DO check max number of mistake."""
-    max_num_mistake = (num_quest * game_level) + 1
-    mistakes = mistake_question()
-    while not mistakes.isdigit():
-        printbox("Invalid input")
-        pause("Press Enter to input number of mistake(s)")
-        mistakes = mistake_question()
-    mistakes = int(mistakes)
-    while mistakes not in range(1, max_num_mistake):
-        print "range", mistakes
-        printbox("Invalid input")
-        pause("Press Enter to input number of mistake(s)")
-        mistakes = mistake_question()
-    return mistakes
+print main()
 
 
-"""if mistakes.isdigit():
-        mistakes = int(mistakes)
-        if mistakes in range(1, max_num_mistake):
-            print "inside", mistakes
-            return mistakes
-        else:
-            printbox("Out of range")
-            pause("Press Enter to input number of mistake(s)")
-            number_of_mistakes(game_level, num_quest)
-    else:
-        printbox("Invalid input")
-        pause("Press Enter to input number of mistake(s)")
-        number_of_mistakes(game_level, num_quest)"""
-
-
-print number_of_mistakes(game_difficulty(), num_question_per_level)
+# print number_of_mistakes(game_difficulty(), num_question_per_level)
